4319181f388977e04ba269262603ecc4f3b63bd8
kylstv <kyleoriana@gmail.com>
2026-03-24 13:51:26 +0800

    asda

--- app.py
+++ app.py
@@ -767,5 +767,8 @@ def admin_report_pdf():
                      mimetype="application/pdf")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
